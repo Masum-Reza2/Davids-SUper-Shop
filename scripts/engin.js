21ff6aa06d5a3b3947d809fe65067bf972057942
Masum-Reza <careerhunter4280@gmail.com>
2023-08-20 03:50:56 -0700

    added reusable functionality

--- scripts/engin.js
+++ scripts/engin.js
@@ -1,8 +1,41 @@
-let itemOneCardOne = document.getElementById('itemOneCardOne');
+//function innerText to number getter
+let innerTextToNum = (id) => {
+    let idName = document.getElementById(id);
+    let text = idName.innerText;
+    let num = Number.parseFloat(text);
+    return num;
+}
 
-itemOneCardOne.addEventListener('click', (()=>{
+// let itemOneCardOne = document.getElementById('itemOneCardOne');
+
+// itemOneCardOne.addEventListener('click', (() => {
+//     //task 01
+//     let itemName = itemOneCardOne.children[1].children[1].innerText;
+//     let selectedItemsParent = document.getElementById('selectedItemsParent');
+//     let li = document.createElement('li');
+//     li.innerText = itemName;
+//     li.classList = 'border text-sm';
+//     selectedItemsParent.appendChild(li);
+
+//     //task 02
+//     let itemPrice = itemOneCardOne.children[1].children[2].innerText.split(' ')[0];
+//     let itemPriceNumber = Number.parseFloat(itemPrice);
+
+//     let totalPrice = document.getElementById('totalPrice');
+//     let totalPriceNum = innerTextToNum('totalPrice');
+
+//     let total = itemPriceNumber + totalPriceNum;
+//     totalPrice.innerText = total.toFixed(2);
+
+// }))
+
+
+
+let myUniversalFunc = (id) => {
+    let idName = document.getElementById(id);
+
     //task 01
-    let itemName = itemOneCardOne.children[1].children[1].innerText;
+    let itemName = idName.children[1].children[1].innerText;
     let selectedItemsParent = document.getElementById('selectedItemsParent');
     let li = document.createElement('li');
     li.innerText = itemName;
@@ -10,16 +43,14 @@ itemOneCardOne.addEventListener('click', (()=>{
     selectedItemsParent.appendChild(li);
 
     //task 02
-    let itemPrice = itemOneCardOne.children[1].children[2].innerText.split(' ')[0];
+    let itemPrice = idName.children[1].children[2].innerText.split(' ')[0];
     let itemPriceNumber = Number.parseFloat(itemPrice);
-    console.log(itemPriceNumber)
 
     let totalPrice = document.getElementById('totalPrice');
-    let totalPriceStr = totalPrice.innerText;
-    let totalPriceNum = Number.parseFloat(totalPriceStr);
-    console.log(totalPriceNum);
+    //here i called my build function
+    let totalPriceNum = innerTextToNum('totalPrice');
 
     let total = itemPriceNumber + totalPriceNum;
-    totalPrice.innerText = total;
+    totalPrice.innerText = total.toFixed(2);
 
-}))
+}
