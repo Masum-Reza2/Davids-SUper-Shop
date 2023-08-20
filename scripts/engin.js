6422eb62d9cc778bdf479e22bb90c72755f251cf
Masum-Reza <careerhunter4280@gmail.com>
2023-08-20 04:03:21 -0700

    calculation total task completed

--- scripts/engin.js
+++ scripts/engin.js
@@ -6,31 +6,11 @@ let innerTextToNum = (id) => {
     return num;
 }
 
-// let itemOneCardOne = document.getElementById('itemOneCardOne');
+// button operations
+let btnApply = document.getElementById('btnApply');
+btnApply.disabled = true;
 
-// itemOneCardOne.addEventListener('click', (() => {
-//     //task 01
-//     let itemName = itemOneCardOne.children[1].children[1].innerText;
-//     let selectedItemsParent = document.getElementById('selectedItemsParent');
-//     let li = document.createElement('li');
-//     li.innerText = itemName;
-//     li.classList = 'border text-sm';
-//     selectedItemsParent.appendChild(li);
-
-//     //task 02
-//     let itemPrice = itemOneCardOne.children[1].children[2].innerText.split(' ')[0];
-//     let itemPriceNumber = Number.parseFloat(itemPrice);
-
-//     let totalPrice = document.getElementById('totalPrice');
-//     let totalPriceNum = innerTextToNum('totalPrice');
-
-//     let total = itemPriceNumber + totalPriceNum;
-//     totalPrice.innerText = total.toFixed(2);
-
-// }))
-
-
-
+// reUseable function onclick 
 let myUniversalFunc = (id) => {
     let idName = document.getElementById(id);
 
